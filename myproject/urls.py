914d0168c7d23d82b713d391ba822c8d28e41b32
”Daniil <lemk@ukr.net>
2018-01-01 22:05:04 +0200

    My first commit

---
+++ myproject/urls.py
@@ -0,0 +1,36 @@
+"""myproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include, patterns
+from django.contrib import admin
+from .settings import MEDIA_ROOT, DEBUG
+
+from lemk.views import main_page
+
+js_info_dict = {
+	'packages': ('lemk',),
+}
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', main_page, 
+	name = 'home'),
+]
+
+if DEBUG:
+	#serve files from media folder
+	urlpatterns += patterns('',
+		url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+			{'document_root': MEDIA_ROOT}))
